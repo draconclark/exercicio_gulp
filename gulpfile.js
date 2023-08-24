1654f697bd327206416a91570d53d13b2ee700b5
draconclark <kelvinlagoa3@gmail.com>
2023-08-24 16:43:31 -0300

    exercicio_gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
+const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin')
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+function compilaJs(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaImg(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
+}
+
+exports.sass = compilaSass
+exports.js = compilaJs
+exports.images = compilaImg
